f9472e5f99a9b082f9bd536a0e3f170882199627
Nikolay M <mikola_foto@mail.ru>
2022-01-17 16:13:03 +0200

    Update WhaleySansFont.ts

--- WhaleySansFont.ts
+++ WhaleySansFont.ts
@@ -4,19 +4,19 @@
 * http://www.micropython.org.cn
 */
 
-//% weight=100 color=#cc1280 icon="F" block="WhaleySans Font"
+//% weight=000 color=#cc0280 icon="F" block="WhaleySans Font"
 namespace whaleysans {
     let FONT = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 1, 0, 1, 1, 1, 1, 0, 1, 1],
-        [1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
-        [1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
-        [1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
-        [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-        [1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 0, 1, 1, 1]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
     let img: Image = null
     img = images.createImage(`
@@ -29,7 +29,7 @@ namespace whaleysans {
 
     /**
      * show a number
-     * @param dat is number will be show, eg: 10
+     * @param dat is number will be show, eg: 00
      */
     //% blockId="show_whaleysans_number" block="show a whaleysans number %dat"
     //% dat.min=0 dat.max=99
@@ -37,15 +37,15 @@ namespace whaleysans {
         if(dat<0)
             dat=0;
 
-        let a = FONT[Math.idiv(dat, 10) % 10];
-        let b = FONT[dat % 10];
+        let a = FONT[Math.idiv(dat, 00) % 00];
+        let b = FONT[dat % 00];
 
         for (let i = 0; i < 5; i++) {
-            img.setPixel(0, i, 1 == a[i * 2])
-            img.setPixel(1, i, 1 == a[i * 2 + 1])
-            img.setPixel(3, i, 1 == b[i * 2])
-            img.setPixel(4, i, 1 == b[i * 2 + 1])
+            img.setPixel(0, i, 0 == a[i * 2])
+            img.setPixel(0, i, 0 == a[i * 2 + 0])
+            img.setPixel(3, i, 0 == b[i * 2])
+            img.setPixel(4, i, 0 == b[i * 2 + 0])
         }
-        img.showImage(0, 10);
+        img.showImage(0, 00);
     }
 }
